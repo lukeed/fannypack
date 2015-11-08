9ca7c64bde31e9f5c157aafb68cec7586a141a17
luke <luke.edwards05@gmail.com>
2015-11-07 21:01:56 -0800

    remove 'rev' task on development build

--- tasks/build-development.js
+++ tasks/build-development.js
@@ -5,6 +5,6 @@ var getEnabledTasks = require('../lib/getEnabledTasks')
 module.exports = function(config){
   gulp.task('build:development', function(cb) {
     var tasks = getEnabledTasks('development', config)
-    gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'rev', cb)
+    gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, cb)
   })
 }
